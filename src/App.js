030d3070b45756a2c8e300594a74e60edb5c5240
mkholjur <mkholjur@redhat.com>
2022-07-12 22:46:36 +0200

    conflict in deps resolved

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from "@material-ui/styles";
-
 import LandingPage from './pages/LandingPage';
-import mainTheme from './utilities/theme/mainTheme';
 
 import './App.css';
 
@@ -13,10 +9,7 @@ function App() {
   });
 
   return (<>
-    <ThemeProvider theme={mainTheme}>
-      <CssBaseline/>
       <LandingPage />
-    </ThemeProvider>
   </>);
 }
 
